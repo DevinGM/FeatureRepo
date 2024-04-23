e80d91770b38292e01d5a2151dc78a7398b1db8e
Devin Monaghan <dgmonaghan7@gmail.com>
2024-04-23 10:30:16 -0700

    Remade Project
    - remade project to URP
    - added Specs script

---
+++ FeatureProject/Assets/Scripts/Specs.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Monaghan, Devin
+/// 4/23/2024
+/// Project Notes
+/// </summary>
+
+public class Specs : MonoBehaviour
+{
+    /*
+     * Heavy Drift - AC6 Tank Movement
+     * 
+     * core mechanic:
+     * 1. player moves back and forth with WS
+     * 2. player strafes with AD and treads rotate to face that way
+     * 3. player boosts forward whatever direction the treads are facing with SPACE
+     * 4. player sprints increasing movement speed by pressing CTRL
+     * 5. player floats up by holding SHIFT and floats back down with gravity
+     * 6. player can only act as long as they have energy, energy recharges on the ground
+     * 7. player enters assault boost mode and boosts foward by pressing R
+     * 
+     */
+}
